6958dc564a502edd6ea45fa8b5a39153300970ba
fancy <1260143408@qq.com>
2017-05-18 20:23:05 +0800

    1.add function intro and system help

--- app/src/main/java/com/monster/fancy/debug/mago/SystemHelpActivity.java
+++ app/src/main/java/com/monster/fancy/debug/mago/SystemHelpActivity.java
@@ -1,5 +1,6 @@
 package com.monster.fancy.debug.mago;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,5 +15,20 @@ public class SystemHelpActivity extends AppCompatActivity {
 
     public void back(View view) {
         finish();
+    }
+
+    public void onPageJump(View view) {
+        int id = view.getId();
+        Intent intent = null;
+        switch (id) {
+            case R.id.funcIntro_text:
+                intent = new Intent(SystemHelpActivity.this, FunctionIntroActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.groupIntro_text:
+                intent = new Intent(SystemHelpActivity.this, GroupIntroActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
